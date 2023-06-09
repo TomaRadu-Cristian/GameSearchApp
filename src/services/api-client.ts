8959f12f62e70c7425771644260ab8f0e9fd5672
TomaRadu-Cristian <100880861+TomaRadu-Cristian@users.noreply.github.com>
2023-06-09 21:18:39 +0300

    Update api-client.ts

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: '4c301c14293a43a79d047d5c16da644a'
+    // insert api key here
+    key: ''
   }
 })
