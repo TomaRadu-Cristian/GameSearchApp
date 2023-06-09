fc8568d2966f1bb6a8a4f7f30800d9c177c54f7f
Toma Cristian <titi.tomita1029@gmail.com>
2023-06-09 16:16:35 +0300

    Sorted games

--- src/services/image-link.ts
+++ src/services/image-link.ts
@@ -1,5 +1,6 @@
 
 const getCroppedImgUrl = (url: string) => {
+  if (!url) return ''
   const media = 'media/'
   const index = url.indexOf(media) + media.length;
   return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
