d5b21745177fb342fa3542ebda047dd1509e58b1
Toma Cristian <titi.tomita1029@gmail.com>
2023-06-09 13:00:42 +0300

    Added genre filter functionality

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,7 +1,12 @@
-import { HStack, Img, List, Spinner, Text } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import { Button, HStack, Img, List, Spinner, Text } from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImgUrl from "../services/image-link";
-const GenreList = () => {
+
+interface Props {
+  onSelectGenres: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenres }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -18,7 +23,13 @@ const GenreList = () => {
               borderRadius={8}
               src={getCroppedImgUrl(genre.image_background)}
             />
-            <Text fontSize={"md"}>{genre.name}</Text>
+            <Button
+              onClick={() => onSelectGenres(genre)}
+              fontSize={"md"}
+              variant="link"
+            >
+              {genre.name}
+            </Button>
           </HStack>
         </List>
       ))}
